abf595fccd69d1b6d90988fb73fcb6ce8c34a042
1alirezahashemi1 <pyalireza7@gmail.com>
2023-04-06 19:04:40 +0430

    convert category to str (many to many field admin problem)

--- Blogsite/config/blog/admin.py
+++ Blogsite/config/blog/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
 from blog.models import Blog , Category
 # Register your models here.
-admin.site.register(Blog)
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ['title','slug','publish','category_to_str']
+    list_filter = ['status']
+    prepopulated_fields = {'slug':('title',)}
+
+    def category_to_str(self,obj):
+        categories = " , ".join([category.title for category in obj.category.all()])
+        if not categories:
+            return "-----"
+        return categories
+        
+
+
+admin.site.register(Blog,BlogAdmin)
 admin.site.register(Category)
