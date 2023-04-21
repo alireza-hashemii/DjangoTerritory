7af9ea34eac256cba697d9fec1bcb2267177eae5
1alirezahashemi1 <pyalireza7@gmail.com>
2023-04-21 06:44:35 +0430

    2 Admin action has been written (published/draft)

--- Blogsite/config/blog/admin.py
+++ Blogsite/config/blog/admin.py
@@ -2,11 +2,27 @@ from django.contrib import admin
 from blog.models import Blog , Category
 # Register your models here.
 
+@admin.action(description="Mark selected stories as published")
+def make_published(modeladmin, request, queryset):
+    queryset.update(status="p")
+
+
+@admin.action(description="Mark selected stories as draft")
+def make_draft(modeladmin, request, queryset):
+    altered_items = queryset.update(status="d")
+    if altered_items ==1:
+        message_bit = "1 blog was"
+    else:
+        message_bit = "%s blogs were" %altered_items
+    modeladmin.message_user(request,"%s updated succesfuly"%message_bit)
+
+
 @admin.register(Blog)
 class BlogAdmin(admin.ModelAdmin):
-    list_display = ['title','slug','jpublish','category_to_str']
+    list_display = ['title','slug','jpublish','status','category_to_str']
     list_filter = ['status']
     prepopulated_fields = {'slug':('title',)}
+    actions = [make_published,make_draft]
 
     def category_to_str(self,obj):
         categories = " , ".join([category.title for category in obj.category_pubished()])
@@ -16,8 +32,7 @@ class BlogAdmin(admin.ModelAdmin):
     category_to_str.short_description = "دسته بندی ها"
     
 
+@admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['title','jpublish','is_active']        
 
-
-admin.site.register(Category,CategoryAdmin)
