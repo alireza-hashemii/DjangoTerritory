1da8d4f537ca09d7358c45c5adfdf2c540b9dc92
1alirezahashemi1 <pyalireza7@gmail.com>
2023-05-19 09:53:45 +0430

    signup

--- instalite/core/splatform/urls.py
+++ instalite/core/splatform/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = "splatform"
 urlpatterns = [
     path("",views.home,name="home"),
     path("signup/",views.signup,name="signup")
--- instalite/core/splatform/views.py
+++ instalite/core/splatform/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import messages
+from .models import Profile
 # Create your views here.
 
 def home(request):
@@ -8,31 +9,27 @@ def home(request):
 
 
 def signup(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        email = request.POST['email']
-        password = request.POST['password']
-        password2 = request.POST['password2']
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+        password2 = request.POST["password2"]
 
         if password == password2:
             if User.objects.filter(email = email).exists():
-                messages.info(request,"Email Taken!")
-                return redirect('core:signup')
-            elif User.objects.filter(username = username).exists():
-                messages.info(request,"Username Taken!")
-                return redirect('core:signup')
+                messages.info(request,"Email Taken")
+                return redirect("splatform:signup")
+            elif User.objects.filter(username=username).exists():
+                messages.info(request,"Username Taken")
+                return redirect("splatform:signup")
             else:
                 user = User.objects.create(username=username,email=email,password=password)
                 user.save()
-                user_object = User.objects.get(username = username)
-                user_profile = Profile.objects.create(username = user_object , user_id = user_object.id)
-                user_profile.save()
-                return redirect('core:index')
-                
-                    
-        else:
-            messages.info(request,"Passwords are not the same")
-            return redirect('core:signup')
-            
+                user_object = User.objects.get(username=username)
+                profile_user = Profile.objects.create(username=user_object,user_id=user_object.id)
+                profile_user.save()
+                messages.info(request,"Alright")
+                return render(request,"index.html")
     else:
-        return render(request,'signup.html')
+        messages.info(request,"Passwords are not the same")
+        return render(request,"signup.html")
